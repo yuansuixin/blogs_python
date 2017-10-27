db0d341e5729e4431cfd5f571cddfe54611707ca
yuan <cyss428@163.com>
2017-10-27 14:03:18 +0800

    first

---
+++ blog/views.py
@@ -0,0 +1,66 @@
+
+import markdown
+from django.shortcuts import render, get_object_or_404
+from comments.forms import CommentForm
+from .models import Post,Category
+from django.db.models.aggregates import Count
+
+
+# Count 计算分类下的文章数，其接受的参数为需要计数的模型的名称
+category_list = Category.objects.annotate(num_posts=Count('post'))
+
+def index(request):
+    post_list = Post.objects.all().order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+def detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    # 记得在顶部引入 markdown 模块
+    post.body = markdown.markdown(post.body,
+                                  extensions=[
+                                     'markdown.extensions.extra',
+                                     'markdown.extensions.codehilite',
+                                     'markdown.extensions.toc',
+                                  ])
+# 记得在顶部导入 CommentForm
+    form = CommentForm()
+    # 获取这篇 post 下的全部评论
+    comment_list = post.comment_set.all()
+
+    # 将文章、表单、以及文章下的评论列表作为模板变量传给 detail.html 模板，以便渲染相应数据。
+    context = {'post': post,
+               'form': form,
+               'comment_list': comment_list
+               }
+    return render(request, 'blog/detail.html', context=context)
+								  
+   
+	
+
+def archives(request, year, month):
+    post_list = Post.objects.filter(created_time__year=year,
+                                    created_time__month=month
+                                    ).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+	
+	
+def category(request, pk):
+    # 记得在开始部分导入 Category 类
+    cate = get_object_or_404(Category, pk=pk)
+    post_list = Post.objects.filter(category=cate).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+	
+	
+	
+def search(request):
+    q = request.GET.get('q')
+    error_msg = ''
+
+    if not q:
+        error_msg = '请输入关键词'
+        return render(request, 'blog/index.html', {'error_msg': error_msg})
+
+    post_list = Post.objects.filter(title__icontains=q)
+    return render(request, 'blog/index.html', {'error_msg': error_msg,
+                                               'post_list': post_list})	
+	
